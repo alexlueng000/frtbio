9f20b2a11f686f9c8d42b2d246dd4190098c5437
AlexLueng <alexlueng000@gmail.com>
2025-05-23 10:20:35 +0800

    optimize

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	
 	<!-- FAVICONS ICON -->
 	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.webp">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
 	
 	<!-- PAGE TITLE HERE -->
 	<title>弗劳恩生物</title>
