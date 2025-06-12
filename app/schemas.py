a1bf39b3f5695d22473b8648741b0072663afefd
AlexLueng <alexlueng000@gmail.com>
2025-06-12 17:10:26 +0800

    fix bug

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 # app/schemas.py
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict, validator
 from typing import Optional, List
 
 
