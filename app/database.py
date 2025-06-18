472e69d1402036776568b5f301619b3ee685aa18
alexlueng <alexlueng000@gmail.com>
2025-06-18 02:02:25 +0000

    update on server

--- app/database.py
+++ app/database.py
@@ -16,6 +16,9 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+# DATABASE_URL = "mysql+pymysql://root:##Szsyjz123456@localhost:3306/bidding_emails"
+
+
 
 engine = create_engine(DATABASE_URL)
 
