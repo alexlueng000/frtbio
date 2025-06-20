31f704679ed4cb50f453772d4bb92c8023e60da6
AlexLueng <alexlueng000@gmail.com>
2025-06-20 09:56:20 +0800

    fix bugs

--- app/database.py
+++ app/database.py
@@ -18,9 +18,12 @@ DB_NAME = os.getenv("DB_NAME")
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 # DATABASE_URL = "mysql+pymysql://root:##Szsyjz123456@localhost:3306/bidding_emails"
 
-
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_recycle=3600,
+    pool_pre_ping=True,
+    echo=False,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
