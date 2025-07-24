19a6c787c0c9c1d68d1485485419d27847e8a149
AlexLueng <alexlueng000@gmail.com>
2025-07-24 15:23:45 +0800

    add seach engine

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="keywords" content="">
 	<meta name="author" content="">
 	<meta name="robots" content="">
-	<meta name="description" content="弗劳恩生物">
+	<meta name="description" content="深圳市弗劳恩生物医学有限公司成立于2015年，是一家专注于生物医学领域的研发、引进先进技术及服务的高科技企业。公司研引并重，前瞻性地在实验室耗材、诊断试剂等国际先进技术及产品进行布局。专注于为全国各地医疗机构、医学院所、生物制药企业提供胎牛血清、生长因子、无血清培养基等高质量产品和临床前研究检测服务。">
 	<meta property="og:title" content="弗劳恩生物">
 	<meta property="og:description" content="弗劳恩生物">
 	<meta name="format-detection" content="telephone=no">
